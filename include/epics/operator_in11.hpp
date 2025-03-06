f2a1b09ca1f25cf5bef974dbf53278779c21f849
ElectronPie <tima001f@gmail.com>
2025-03-06 14:40:30 +0300

    Added Doxygen support

--- include/epics/operator_in11.hpp
+++ include/epics/operator_in11.hpp
@@ -4,30 +4,75 @@
 
 namespace eps
 {
+    /// @cond SHOW_INTERNAL
+    /**
+     * @brief Holds a reference to a value before it's checked for appearing in a container.
+     *
+     * @tparam T The value type
+     */
     template<typename T>
     struct __operator_in_lhs
     {
     public:
+        /**
+         * @brief Construct a new __operator_in_lhs object
+         *
+         * @param val The value reference.
+         */
         __operator_in_lhs(const T& val): val{val}
         {}
 
+        /**
+         * @brief The value reference.
+         *
+         */
         const T& val;
     };
 
-    template<typename T, typename C, typename... Args>
+    /**
+     * @brief Checks whether a value from __operator_in_lhs<T> occurs in a container.
+     *
+     * @tparam T The value type.
+     * @tparam C The container type.
+     * @param lhs __operator_in_lhs<T> struct hosting the value.
+     * @param c The container.
+     * @return bool If the value occurs in the container.
+     */
+    template<typename T, typename C>
     bool operator|(__operator_in_lhs<T> lhs, const C& c)
     {
         return std::find(std::begin(c), std::end(c), lhs.val) != std::end(c);
     }
 
+    /**
+     * @brief Intermediate struct hosting the operator to wrap a value into __operator_in_lhs.
+     *
+     */
     struct __operator_in
     {};
 
+    /**
+     * @brief Wraps a value into __operator_in_lhs.
+     *
+     * @tparam T The value type.
+     * @param lhs The value.
+     * @param rhs __operator_in intermediate struct.
+     * @return __operator_in_lhs<T>
+     */
     template<typename T>
     __operator_in_lhs<T> operator|(const T& lhs, __operator_in rhs)
     {
         return __operator_in_lhs{lhs};
     }
+
+    /// @endcond
 } // namespace eps
 
+/**
+ * @brief The operator in
+ *
+ * A macro analog of the operator `in` that, given a value and a container,
+ * returns a boolean indicating whether the value occurs in the container.
+ *
+ */
 #define in | eps::__operator_in{} |
--- include/epics/pstream17.hpp
+++ include/epics/pstream17.hpp
@@ -8,22 +8,46 @@
 
 namespace eps
 {
+    /**
+     * @brief Base class for the pstreams.
+     *
+     */
     class pstream_base
     {
     protected:
         static inline std::mutex g_mtx;
     };
 
+    /**
+     * @brief Thread-safe wrapper type for input streams.
+     *
+     * @tparam istream_t The input stream type.
+     */
     template<typename istream_t>
     class pistream: private pstream_base
     {
     private:
+        /// @cond SHOW_INTERNAL
+        /**
+         * @brief Accumulates I/O operations to perform atomically.
+         *
+         */
         class __pistream_temp: pstream_base
         {
         public:
+            /**
+             * @brief Construct a new pistream temp object.
+             *
+             * @param istream Reference to the original input stream.
+             */
             __pistream_temp(istream_t& istream): m_istream{istream}
             {}
 
+            /**
+             * @brief Move constructor.
+             *
+             * @param other The other __pistream_temp object.
+             */
             __pistream_temp(__pistream_temp&& other): m_istream{other.m_istream}, m_actions{std::move(other.m_actions)}
             {
                 while (!other.m_actions.empty())
@@ -32,6 +56,10 @@ namespace eps
                 }
             }
 
+            /**
+             * @brief Destroy the __pistream_temp object performing the accumulated I/O operations atomically.
+             *
+             */
             ~__pistream_temp()
             {
                 std::lock_guard<std::mutex> lk{g_mtx};
@@ -42,6 +70,13 @@ namespace eps
                 }
             }
 
+            /**
+             * @brief Add a new I/O operation to the queue.
+             *
+             * @tparam T The read value type.
+             * @param val Stores the value read after performing the action.
+             * @return __pistream_temp Moved xvalue of self.
+             */
             template<typename T>
             __pistream_temp operator>>(T& val)
             {
@@ -51,17 +86,38 @@ namespace eps
             }
 
         private:
+            /**
+             * @brief Reference to the original input stream.
+             *
+             */
             istream_t& m_istream;
+            /**
+             * @brief Queue of the I/O operations performed upon the object destruction.
+             *
+             */
             std::queue<std::function<void()>> m_actions;
         };
 
     protected:
         istream_t& m_istream;
+        /// @endcond
 
     public:
+        /**
+         * @brief Construct a new pistream object.
+         *
+         * @param istream Reference to the original input stream.
+         */
         explicit pistream(istream_t& istream): m_istream{istream}
         {}
 
+        /**
+         * @brief Reads a value after the chain of \>\>s is completed.
+         *
+         * @tparam T The read value type.
+         * @param val Reference to the value read.
+         * @return __pistream_temp Temporary object to continue the chain of \>\>s.
+         */
         template<typename T>
         __pistream_temp operator>>(T& val)
         {
@@ -69,16 +125,36 @@ namespace eps
         };
     };
 
+    /**
+     * @brief Thread-safe wrapper type for output streams.
+     *
+     * @tparam ostream_t The output stream type.
+     */
     template<typename ostream_t>
     class postream: pstream_base
     {
     private:
+        /// @cond SHOW_INTERNAL
+        /**
+         * @brief Accumulates I/O operations to perform atomically.
+         *
+         */
         class __postream_temp
         {
         public:
+            /**
+             * @brief Construct a new __postream_temp object.
+             *
+             * @param ostream Reference to the original output stream.
+             */
             __postream_temp(ostream_t& ostream): m_ostream{ostream}
             {}
 
+            /**
+             * @brief Move constructor.
+             *
+             * @param other The other __postream_temp object.
+             */
             __postream_temp(__postream_temp&& other): m_ostream{other.m_ostream}, m_actions{std::move(other.m_actions)}
             {
                 while (!other.m_actions.empty())
@@ -87,6 +163,10 @@ namespace eps
                 }
             }
 
+            /**
+             * @brief Destroy the __postream_temp object performing the accumulated I/O operations atomically.
+             *
+             */
             ~__postream_temp()
             {
                 std::lock_guard<std::mutex> lk{g_mtx};
@@ -97,6 +177,13 @@ namespace eps
                 }
             }
 
+            /**
+             * @brief Add a new I/O operation to the queue.
+             *
+             * @tparam T The written value type.
+             * @param val The value to write.
+             * @return __postream_temp Moved xvalue of self.
+             */
             template<typename T>
             __postream_temp operator<<(T& val)
             {
@@ -106,17 +193,42 @@ namespace eps
             }
 
         private:
+            /**
+             * @brief Reference to the original output stream.
+             *
+             */
             ostream_t& m_ostream;
+            /**
+             * @brief Queue of the I/O operations performed upon the object destruction.
+             *
+             */
             std::queue<std::function<void()>> m_actions;
         };
 
     protected:
+        /**
+         * @brief Reference to the original output stream.
+         *
+         */
         ostream_t& m_ostream;
+        /// @endcond
 
     public:
+        /**
+         * @brief Construct a new postream object.
+         *
+         * @param ostream Reference to the original output stream.
+         */
         explicit postream(ostream_t& ostream): m_ostream{ostream}
         {}
 
+        /**
+         * @brief Writes a value after the chain of \<\<s is completed.
+         *
+         * @tparam T The written value type.
+         * @param val The value to write.
+         * @return __postream_temp Temporary object to continue the chain of \<\<s.
+         */
         template<typename T>
         __postream_temp operator<<(T& val)
         {
@@ -124,16 +236,38 @@ namespace eps
         }
     };
 
+    /**
+     * @brief Thread-safe wrapper type for input/output streams.
+     *
+     * @tparam istream_t Input stream interface type.
+     * @tparam ostream_t Output stream interface type.
+     */
     template<typename istream_t, typename ostream_t>
     class pstream: private pstream_base
     {
     private:
+        /// @cond SHOW_INTERNAL
+        /**
+         * @brief Accumulates I/O operations to perform atomically.
+         *
+         */
         class __pstream_temp: private pstream_base
         {
         public:
+            /**
+             * @brief Construct a new __pstream_temp object
+             *
+             * @param istream Reference to the original input stream.
+             * @param ostream Reference to the original output stream.
+             */
             __pstream_temp(istream_t& istream, ostream_t& ostream): m_istream{istream}, m_ostream{ostream}
             {}
 
+            /**
+             * @brief Move constructor.
+             *
+             * @param other The other __pstream_temp object.
+             */
             __pstream_temp(__pstream_temp&& other):
                 m_istream{other.m_istream}, m_ostream{other.m_ostream}, m_actions{std::move(other.m_actions)}
             {
@@ -143,6 +277,10 @@ namespace eps
                 }
             }
 
+            /**
+             * @brief Destroy the __pstream_temp object performing the accumulated I/O operations atomically.
+             *
+             */
             ~__pstream_temp()
             {
                 std::lock_guard<std::mutex> lk{g_mtx};
@@ -153,6 +291,13 @@ namespace eps
                 }
             }
 
+            /**
+             * @brief Add a new I/O operation to the queue.
+             *
+             * @tparam T The read value type.
+             * @param val Reference to the value read.
+             * @return __postream_temp Moved xvalue of self.
+             */
             template<typename T>
             __pstream_temp operator>>(T& val)
             {
@@ -161,6 +306,13 @@ namespace eps
                 return std::move(*this);
             }
 
+            /**
+             * @brief Add a new I/O operation to the queue.
+             *
+             * @tparam T The written value type.
+             * @param val The value to write.
+             * @return __postream_temp Moved xvalue of self.
+             */
             template<typename T>
             __pstream_temp operator<<(T& val)
             {
@@ -170,35 +322,86 @@ namespace eps
             }
 
         private:
+            /**
+             * @brief Reference to the original input stream.
+             *
+             */
             istream_t& m_istream;
+            /**
+             * @brief Reference to the original output stream.
+             *
+             */
             ostream_t& m_ostream;
+            /**
+             * @brief Queue of the I/O operations performed upon the object destruction.
+             *
+             */
             std::queue<std::function<void()>> m_actions;
         };
 
     protected:
+        /**
+         * @brief Reference to the original input stream.
+         *
+         */
         istream_t& m_istream;
+        /**
+         * @brief Reference to the original output stream.
+         *
+         */
         ostream_t& m_ostream;
+        ///@endcond
 
     public:
+        /**
+         * @brief Construct a new pstream object.
+         *
+         * @param istream Reference to the original input stream.
+         * @param ostream Reference to the original output stream.
+         */
         explicit pstream(istream_t& istream, ostream_t& ostream): m_istream{istream}, m_ostream{ostream}
         {}
 
+        /**
+         * @brief Cast to pistream<istream_t>.
+         *
+         * @return pistream<istream_t>
+         */
         operator pistream<istream_t>()
         {
             return pistream{m_istream};
         }
 
+        /**
+         * @brief Cast to postream<ostream_t>.
+         *
+         * @return postream<ostream_t>
+         */
         operator postream<ostream_t>()
         {
             return postream{m_ostream};
         }
 
+        /**
+         * @brief Reads a value after the chain of \<\<s and \>\>s is completed.
+         *
+         * @tparam T The read value type.
+         * @param val Reference to the value read.
+         * @return __pistream_temp Temporary object to continue the chain of \<\<s and \>\>s.
+         */
         template<typename T>
         __pstream_temp operator>>(T& val)
         {
             return __pstream_temp{m_istream, m_ostream} >> val;
         }
 
+        /**
+         * @brief Writes a value after the chain of \<\<s and \>\>s is completed.
+         *
+         * @tparam T The written value type.
+         * @param val The value to write.
+         * @return __pstream_temp Temporary object to continue the chain of \<\<s and \>\>s.
+         */
         template<typename T>
         __pstream_temp operator<<(T& val)
         {
@@ -206,18 +409,58 @@ namespace eps
         }
     };
 
+    /**
+     * @brief Thread-safe `std::cin` wrapper.
+     *
+     */
     inline pistream<std::istream> pcin{std::cin};
+    /**
+     * @brief Thread-safe `std::wcin` wrapper.
+     *
+     */
     inline pistream<std::wistream> pwcin{std::wcin};
 
+    /**
+     * @brief Thread-safe `std::cout` wrapper.
+     *
+     */
     inline postream<std::ostream> pcout{std::cout};
+    /**
+     * @brief Thread-safe `std::wcout` wrapper.
+     *
+     */
     inline postream<std::wostream> pwcout{std::wcout};
 
+    /**
+     * @brief Thread-safe `std::cerr` wrapper.
+     *
+     */
     inline postream<std::ostream> pcerr{std::cerr};
+    /**
+     * @brief Thread-safe `std::wcerr` wrapper.
+     *
+     */
     inline postream<std::wostream> pwcerr{std::wcerr};
 
+    /**
+     * @brief Thread-safe `std::clog` wrapper.
+     *
+     */
     inline postream<std::ostream> pclog{std::clog};
+    /**
+     * @brief Thread-safe `std::wclog` wrapper.
+     *
+     */
     inline postream<std::wostream> pwclog{std::wclog};
 
+    /**
+     * @brief Thread-safe I/O wrapper for `std::cin` and `std::cout`.
+     *
+     */
     inline pstream<std::istream, std::ostream> pcio{std::cin, std::cout};
+    /**
+     * @brief Thread-safe I/O wrapper for `std::wcin` and `std::wcout`.
+     *
+     */
     inline pstream<std::wistream, std::wostream> pwcio{std::wcin, std::wcout};
 } // namespace eps
